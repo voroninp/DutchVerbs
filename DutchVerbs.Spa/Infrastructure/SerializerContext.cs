ced76ec2e8a95dbcb2563e3cbbb07826651a3bec
Pavel Voronin <voroninp@gmail.com>
2024-11-13 21:24:30 +0100

    Enable AOT

--- DutchVerbs.Spa/Infrastructure/SerializerContext.cs
+++ DutchVerbs.Spa/Infrastructure/SerializerContext.cs
@@ -4,7 +4,6 @@ using DutchVerbs.Spa.Infrastructure.DTOs;
 namespace DutchVerbs.Spa.Infrastructure;
 
 [JsonSerializable(typeof(ApplicationStateDto))]
-[JsonSerializable(typeof(object))]
 [JsonSourceGenerationOptions(WriteIndented = true)]
 public sealed partial class SourceGenerationContext : JsonSerializerContext
 {
